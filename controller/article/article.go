4f55ea796800f515cd4765509fed83e20a0c9f3d
y-ytakahashi <ty.ytakahashi.work@gmail.com>
2021-05-17 00:12:37 +0900

    fix #2 フロントデータ整形処理の調査

--- controller/article/article.go
+++ controller/article/article.go
@@ -1,7 +1,7 @@
 package article
 import (
 	"fmt"
-	"log"
+	// "log"
 	"github.com/gin-gonic/gin"
 	"readinglist-backend-api/service/article"
 	// debug
@@ -27,13 +27,20 @@ func (pc Controller) Index(c *gin.Context) {
 // Create action: POST /article
 func (pc Controller) Create(c *gin.Context) {
 	var s article.Service
-	p, err := s.CreateModel(c)
+
+
+
+	r, err := s.RequestParse(c)
+
+	// p, err := s.CreateModel(c)
+
 
 	if err != nil {
 			c.AbortWithStatus(400)
 			fmt.Println(err)
 	} else {
-			c.JSON(201, p)
+			// c.JSON(201, p)
+			c.JSON(201, r)
 	}
 }
 
